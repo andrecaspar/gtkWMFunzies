45fa2f3a0be80ac3a743437bbdbaa3a0fdf02ad8
andrecaspar <23707164+andrecaspar@users.noreply.github.com>
2022-09-04 02:01:01 -0300

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -7,6 +7,11 @@
 #include <xcb/xcb.h>
 #include <xcb/xcb_cursor.h>
 
+static xcb_connection_t *dpy;
+static xcb_screen_t *scre;
+static xcb_drawable_t win;
+static uint32_t values[3];
+
 class WindowMain : public Gtk::Window {
 public:
   void On_button_clicked() { std::cout << "test" << std::endl; }
@@ -41,6 +46,55 @@ public:
 private:
   Gtk::Button button1;
 };
+
+static void handleMotionNotify(xcb_generic_event_t *ev) {
+  xcb_query_pointer_cookie_t coord = xcb_query_pointer(dpy, scre->root);
+  xcb_query_pointer_reply_t *poin = xcb_query_pointer_reply(dpy, coord, 0);
+  if ((values[2] == (uint32_t)(1)) && (win != 0)) {
+    xcb_get_geometry_cookie_t geom_now = xcb_get_geometry(dpy, win);
+    xcb_get_geometry_reply_t *geom =
+        xcb_get_geometry_reply(dpy, geom_now, NULL);
+    uint16_t geom_x = geom->width + (2 * 1);
+    uint16_t geom_y = geom->height + (2 * 1);
+    values[0] = ((poin->root_x + geom_x) > scre->width_in_pixels)
+                    ? (scre->width_in_pixels - geom_x)
+                    : poin->root_x;
+    values[1] = ((poin->root_y + geom_y) > scre->height_in_pixels)
+                    ? (scre->height_in_pixels - geom_y)
+                    : poin->root_y;
+    xcb_configure_window(dpy, win, XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y,
+                         values);
+  } else if ((values[2] == (uint32_t)(3)) && (win != 0)) {
+    xcb_get_geometry_cookie_t geom_now = xcb_get_geometry(dpy, win);
+    xcb_get_geometry_reply_t *geom =
+        xcb_get_geometry_reply(dpy, geom_now, NULL);
+    if (!((poin->root_x <= geom->x) || (poin->root_y <= geom->y))) {
+      values[0] = poin->root_x - geom->x - 1;
+      values[1] = poin->root_y - geom->y - 1;
+      if ((values[0] >= (uint32_t)(10)) && (values[1] >= (uint32_t)(10))) {
+        xcb_configure_window(dpy, win,
+                             XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT,
+                             values);
+      }
+    }
+  } else {
+  }
+}
+
+static void handleButtonPress(xcb_generic_event_t *ev) {
+  printf("\n\n\n\n\n\nttttttttttttttttttt");
+  xcb_button_press_event_t *e = (xcb_button_press_event_t *)ev;
+  win = e->child;
+  values[0] = XCB_STACK_MODE_ABOVE;
+  xcb_configure_window(dpy, win, XCB_CONFIG_WINDOW_STACK_MODE, values);
+  values[2] = ((1 == e->detail) ? 1 : ((win != 0) ? 3 : 0));
+  xcb_grab_pointer(dpy, 0, scre->root,
+                   XCB_EVENT_MASK_BUTTON_RELEASE |
+                       XCB_EVENT_MASK_BUTTON_MOTION |
+                       XCB_EVENT_MASK_POINTER_MOTION_HINT,
+                   XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC, scre->root,
+                   XCB_NONE, XCB_CURRENT_TIME);
+}
 
 void test(int argc, char *argv[]) {
   auto application = Gtk::Application::create("test");
@@ -85,90 +139,46 @@ void test2() {
 
   xcb_flush(connection);
 
+  dpy = connection;
+  scre = screen;
+
   xcb_generic_event_t *event;
   while ((event = xcb_wait_for_event(connection))) {
-    switch (event->response_type & ~0x80) {
+    switch (event->response_type) {
     case XCB_EXPOSE: {
       xcb_expose_event_t *expose = (xcb_expose_event_t *)event;
-
-      printf("Window %" PRIu32
-             " exposed. Region to be redrawn at location (%" PRIu16 ",%" PRIu16
-             "), with dimension (%" PRIu16 ",%" PRIu16 ")\n",
-             expose->window, expose->x, expose->y, expose->width,
-             expose->height);
       break;
     }
     case XCB_BUTTON_PRESS: {
-      xcb_button_press_event_t *bp = (xcb_button_press_event_t *)event;
-
-      xcb_configure_window(connection,bp->event, XCB_CONFIG_WINDOW_X, &bp->root_x);
-
-      switch (bp->detail) {
-      case 4:
-        printf("Wheel Button up in window %" PRIu32 ", at coordinates (%" PRIi16
-               ",%" PRIi16 ")\n",
-               bp->event, bp->event_x, bp->event_y);
-        break;
-      case 5:
-        printf("Wheel Button down in window %" PRIu32
-               ", at coordinates (%" PRIi16 ",%" PRIi16 ")\n",
-               bp->event, bp->event_x, bp->event_y);
-        break;
-      default:
-        printf("Button %" PRIu8 " pressed in window %" PRIu32
-               ", at coordinates (%" PRIi16 ",%" PRIi16 ")\n",
-               bp->detail, bp->event, bp->event_x, bp->event_y);
-        break;
-      }
+      printf("\n\n\n\n\n\nttttttttttttttttttt");
+      handleButtonPress(event);
       break;
     }
     case XCB_BUTTON_RELEASE: {
       xcb_button_release_event_t *br = (xcb_button_release_event_t *)event;
-
-      printf("Button %" PRIu8 " released in window %" PRIu32
-             ", at coordinates (%" PRIi16 ",%" PRIi16 ")\n",
-             br->detail, br->event, br->event_x, br->event_y);
       break;
     }
     case XCB_MOTION_NOTIFY: {
-      xcb_motion_notify_event_t *motion = (xcb_motion_notify_event_t *)event;
-
-      printf("Mouse moved in window %" PRIu32 ", at coordinates (%" PRIi16
-             ",%" PRIi16 ")\n",
-             motion->event, motion->event_x, motion->event_y);
+      handleMotionNotify(event);
       break;
     }
     case XCB_ENTER_NOTIFY: {
       xcb_enter_notify_event_t *enter = (xcb_enter_notify_event_t *)event;
-
-      printf("Mouse entered window %" PRIu32 ", at coordinates (%" PRIi16
-             ",%" PRIi16 ")\n",
-             enter->event, enter->event_x, enter->event_y);
       break;
     }
     case XCB_LEAVE_NOTIFY: {
       xcb_leave_notify_event_t *leave = (xcb_leave_notify_event_t *)event;
-
-      printf("Mouse left window %" PRIu32 ", at coordinates (%" PRIi16
-             ",%" PRIi16 ")\n",
-             leave->event, leave->event_x, leave->event_y);
       break;
     }
     case XCB_KEY_PRESS: {
       xcb_key_press_event_t *kp = (xcb_key_press_event_t *)event;
-
-      printf("Key pressed in window %" PRIu32 "\n", kp->event);
       break;
     }
     case XCB_KEY_RELEASE: {
       xcb_key_release_event_t *kr = (xcb_key_release_event_t *)event;
-
-      printf("Key released in window %" PRIu32 "\n", kr->event);
       break;
     }
     default:
-      /* Unknown event type, ignore it */
-      printf("Unknown event: %" PRIu8 "\n", event->response_type);
       break;
     }
 
