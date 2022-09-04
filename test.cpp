ec7efea9d8db04f954931b0d6da9a7b9135666fc
andrecaspar <23707164+andrecaspar@users.noreply.github.com>
2022-09-04 17:30:25 -0300

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -6,6 +6,7 @@
 #include <thread>
 #include <xcb/xcb.h>
 #include <xcb/xcb_cursor.h>
+#include <xcb/xcb_keysyms.h>
 
 static xcb_connection_t *dpy;
 static xcb_screen_t *scre;
@@ -47,21 +48,12 @@ private:
   Gtk::Button button1;
 };
 
-static void handleMotionNotify(xcb_generic_event_t *ev) {
-  values[0] = 1000;
-  values[1] = 200;
-  xcb_configure_window(dpy, win, XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y,
-                       values);
-  
-	xcb_flush(dpy);
-}
-
 static void handleButtonPress(xcb_generic_event_t *ev) {
   xcb_button_press_event_t *e = (xcb_button_press_event_t *)ev;
   win = e->child;
   values[0] = XCB_STACK_MODE_ABOVE;
   xcb_configure_window(dpy, win, XCB_CONFIG_WINDOW_STACK_MODE, values);
-  values[2] = (1 == e->detail) ? 1 : 3;
+  values[2] = ((1 == e->detail) ? 1 : ((win != 0) ? 3 : 0));
   xcb_grab_pointer(dpy, 0, scre->root,
                    XCB_EVENT_MASK_BUTTON_RELEASE |
                        XCB_EVENT_MASK_BUTTON_MOTION |
@@ -70,18 +62,64 @@ static void handleButtonPress(xcb_generic_event_t *ev) {
                    XCB_NONE, XCB_CURRENT_TIME);
 }
 
-void test(int argc, char *argv[]) {
-  auto application = Gtk::Application::create("test");
-  application->make_window_and_run<WindowMain>(argc, argv);
+static void handleMotionNotify(xcb_generic_event_t *ev) {
+  xcb_query_pointer_cookie_t coord = xcb_query_pointer(dpy, scre->root);
+  xcb_query_pointer_reply_t *poin = xcb_query_pointer_reply(dpy, coord, 0);
+  if ((values[2] == (uint32_t)(1)) && (win != 0)) {
+    xcb_get_geometry_cookie_t geom_now = xcb_get_geometry(dpy, win);
+    xcb_get_geometry_reply_t *geom =
+        xcb_get_geometry_reply(dpy, geom_now, NULL);
+    uint16_t geom_x = geom->width + (2);
+    uint16_t geom_y = geom->height + (2);
+    values[0] = ((poin->root_x + geom_x) > scre->width_in_pixels)
+                    ? (scre->width_in_pixels - geom_x)
+                    : poin->root_x;
+    values[1] = ((poin->root_y + geom_y) > scre->height_in_pixels)
+                    ? (scre->height_in_pixels - geom_y)
+                    : poin->root_y;
+    xcb_configure_window(dpy, win, XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y,
+                         values);
+  } else if ((values[2] == (uint32_t)(3)) && (win != 0)) {
+    xcb_get_geometry_cookie_t geom_now = xcb_get_geometry(dpy, win);
+    xcb_get_geometry_reply_t *geom =
+        xcb_get_geometry_reply(dpy, geom_now, NULL);
+    if (!((poin->root_x <= geom->x) || (poin->root_y <= geom->y))) {
+      values[0] = poin->root_x - geom->x;
+      values[1] = poin->root_y - geom->y;
+      if ((values[0] >= (uint32_t)(10)) && (values[1] >= (uint32_t)(10))) {
+        xcb_configure_window(dpy, win,
+                             XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT,
+                             values);
+      }
+    }
+  } else {
+  }
 }
 
-void test2() {
-  dpy = xcb_connect(NULL, NULL);
+void setup(void) {
+  values[0] =
+      XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT | XCB_EVENT_MASK_STRUCTURE_NOTIFY |
+      XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY | XCB_EVENT_MASK_PROPERTY_CHANGE;
+  xcb_change_window_attributes_checked(dpy, scre->root, XCB_CW_EVENT_MASK,
+                                       values);
+  xcb_ungrab_key(dpy, XCB_GRAB_ANY, scre->root, XCB_MOD_MASK_ANY);
+  xcb_flush(dpy);
+  xcb_grab_button(dpy, 0, scre->root,
+                  XCB_EVENT_MASK_BUTTON_PRESS | XCB_EVENT_MASK_BUTTON_RELEASE,
+                  XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC, scre->root,
+                  XCB_NONE, 1, XCB_MOD_MASK_4);
+  xcb_grab_button(dpy, 0, scre->root,
+                  XCB_EVENT_MASK_BUTTON_PRESS | XCB_EVENT_MASK_BUTTON_RELEASE,
+                  XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC, scre->root,
+                  XCB_NONE, 3, XCB_MOD_MASK_4);
+  xcb_flush(dpy);
+}
 
-  /* Get the first scre */
+void xThread(int argc, char *argv[]) {
+  dpy = xcb_connect(NULL, 0);
   scre = xcb_setup_roots_iterator(xcb_get_setup(dpy)).data;
+  setup();
 
-  /* Create the window */
   xcb_window_t window = xcb_generate_id(dpy);
 
   uint32_t mask = XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK;
@@ -94,14 +132,13 @@ void test2() {
 
   xcb_create_window(dpy, 0,                        /* depth               */
                     window, scre->root,            /* parent window       */
-                    0, 0,                          /* x, y                */
+                    150, 150,                      /* x, y                */
                     150, 150,                      /* width, height       */
                     10,                            /* border_width        */
                     XCB_WINDOW_CLASS_INPUT_OUTPUT, /* class               */
                     scre->root_visual,             /* visual              */
                     mask, values);                 /* masks */
 
-  /* Map the window on the scre */
   xcb_map_window(dpy, window);
 
   xcb_cursor_context_t *pcursorctx;
@@ -114,53 +151,30 @@ void test2() {
   xcb_generic_event_t *event;
   while ((event = xcb_wait_for_event(dpy))) {
     switch (event->response_type) {
-    case XCB_EXPOSE: {
-      xcb_expose_event_t *expose = (xcb_expose_event_t *)event;
-      break;
-    }
     case XCB_BUTTON_PRESS: {
       handleButtonPress(event);
-      break;
-    }
-    case XCB_BUTTON_RELEASE: {
-      xcb_button_release_event_t *br = (xcb_button_release_event_t *)event;
       break;
     }
     case XCB_MOTION_NOTIFY: {
       handleMotionNotify(event);
       break;
     }
-    case XCB_ENTER_NOTIFY: {
-      xcb_enter_notify_event_t *enter = (xcb_enter_notify_event_t *)event;
-      break;
-    }
-    case XCB_LEAVE_NOTIFY: {
-      xcb_leave_notify_event_t *leave = (xcb_leave_notify_event_t *)event;
-      break;
-    }
-    case XCB_KEY_PRESS: {
-      xcb_key_press_event_t *kp = (xcb_key_press_event_t *)event;
-      break;
-    }
-    case XCB_KEY_RELEASE: {
-      xcb_key_release_event_t *kr = (xcb_key_release_event_t *)event;
-      break;
-    }
-    default:
-      break;
     }
 
     free(event);
   }
 }
 
+void gtkThread(int argc, char *argv[]) {
+  auto application = Gtk::Application::create("hmm");
+  application->make_window_and_run<WindowMain>(argc, argv);
+}
+
 int main(int argc, char *argv[]) {
-
-  std::thread threadTest(test, argc, argv);
-  std::thread threadTest2(test2);
-
-  threadTest.join();
+  std::thread threadTest(gtkThread, argc, argv);
+  std::thread threadTest2(xThread, argc, argv);
   threadTest2.join();
+  threadTest.join();
 
   return 0;
 }
