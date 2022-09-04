b8756dfdfcd2cd01a7f613929e6c3c9f4d254cde
andrecaspar <23707164+andrecaspar@users.noreply.github.com>
2022-09-04 09:35:14 -0300

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -48,46 +48,20 @@ private:
 };
 
 static void handleMotionNotify(xcb_generic_event_t *ev) {
-  xcb_query_pointer_cookie_t coord = xcb_query_pointer(dpy, scre->root);
-  xcb_query_pointer_reply_t *poin = xcb_query_pointer_reply(dpy, coord, 0);
-  if ((values[2] == (uint32_t)(1)) && (win != 0)) {
-    xcb_get_geometry_cookie_t geom_now = xcb_get_geometry(dpy, win);
-    xcb_get_geometry_reply_t *geom =
-        xcb_get_geometry_reply(dpy, geom_now, NULL);
-    uint16_t geom_x = geom->width + (2 * 1);
-    uint16_t geom_y = geom->height + (2 * 1);
-    values[0] = ((poin->root_x + geom_x) > scre->width_in_pixels)
-                    ? (scre->width_in_pixels - geom_x)
-                    : poin->root_x;
-    values[1] = ((poin->root_y + geom_y) > scre->height_in_pixels)
-                    ? (scre->height_in_pixels - geom_y)
-                    : poin->root_y;
-    xcb_configure_window(dpy, win, XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y,
-                         values);
-  } else if ((values[2] == (uint32_t)(3)) && (win != 0)) {
-    xcb_get_geometry_cookie_t geom_now = xcb_get_geometry(dpy, win);
-    xcb_get_geometry_reply_t *geom =
-        xcb_get_geometry_reply(dpy, geom_now, NULL);
-    if (!((poin->root_x <= geom->x) || (poin->root_y <= geom->y))) {
-      values[0] = poin->root_x - geom->x - 1;
-      values[1] = poin->root_y - geom->y - 1;
-      if ((values[0] >= (uint32_t)(10)) && (values[1] >= (uint32_t)(10))) {
-        xcb_configure_window(dpy, win,
-                             XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT,
-                             values);
-      }
-    }
-  } else {
-  }
+  values[0] = 1000;
+  values[1] = 200;
+  xcb_configure_window(dpy, win, XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y,
+                       values);
+  
+	xcb_flush(dpy);
 }
 
 static void handleButtonPress(xcb_generic_event_t *ev) {
-  printf("\n\n\n\n\n\nttttttttttttttttttt");
   xcb_button_press_event_t *e = (xcb_button_press_event_t *)ev;
   win = e->child;
   values[0] = XCB_STACK_MODE_ABOVE;
   xcb_configure_window(dpy, win, XCB_CONFIG_WINDOW_STACK_MODE, values);
-  values[2] = ((1 == e->detail) ? 1 : ((win != 0) ? 3 : 0));
+  values[2] = (1 == e->detail) ? 1 : 3;
   xcb_grab_pointer(dpy, 0, scre->root,
                    XCB_EVENT_MASK_BUTTON_RELEASE |
                        XCB_EVENT_MASK_BUTTON_MOTION |
@@ -102,55 +76,49 @@ void test(int argc, char *argv[]) {
 }
 
 void test2() {
-  xcb_connection_t *connection = xcb_connect(NULL, NULL);
+  dpy = xcb_connect(NULL, NULL);
 
-  /* Get the first screen */
-  xcb_screen_t *screen =
-      xcb_setup_roots_iterator(xcb_get_setup(connection)).data;
+  /* Get the first scre */
+  scre = xcb_setup_roots_iterator(xcb_get_setup(dpy)).data;
 
   /* Create the window */
-  xcb_window_t window = xcb_generate_id(connection);
+  xcb_window_t window = xcb_generate_id(dpy);
 
   uint32_t mask = XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK;
   uint32_t values[2] = {
-      screen->white_pixel,
+      scre->white_pixel,
       XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_BUTTON_PRESS |
           XCB_EVENT_MASK_BUTTON_RELEASE | XCB_EVENT_MASK_POINTER_MOTION |
           XCB_EVENT_MASK_ENTER_WINDOW | XCB_EVENT_MASK_LEAVE_WINDOW |
           XCB_EVENT_MASK_KEY_PRESS | XCB_EVENT_MASK_KEY_RELEASE};
 
-  xcb_create_window(connection, 0,                 /* depth               */
-                    window, screen->root,          /* parent window       */
+  xcb_create_window(dpy, 0,                        /* depth               */
+                    window, scre->root,            /* parent window       */
                     0, 0,                          /* x, y                */
                     150, 150,                      /* width, height       */
                     10,                            /* border_width        */
                     XCB_WINDOW_CLASS_INPUT_OUTPUT, /* class               */
-                    screen->root_visual,           /* visual              */
+                    scre->root_visual,             /* visual              */
                     mask, values);                 /* masks */
 
-  /* Map the window on the screen */
-  xcb_map_window(connection, window);
+  /* Map the window on the scre */
+  xcb_map_window(dpy, window);
 
   xcb_cursor_context_t *pcursorctx;
-  xcb_cursor_context_new(connection, screen, &pcursorctx);
+  xcb_cursor_context_new(dpy, scre, &pcursorctx);
   xcb_cursor_t cursor = xcb_cursor_load_cursor(pcursorctx, "left_ptr");
-  xcb_change_window_attributes(connection, screen->root, XCB_CW_CURSOR,
-                               &cursor);
+  xcb_change_window_attributes(dpy, scre->root, XCB_CW_CURSOR, &cursor);
 
-  xcb_flush(connection);
-
-  dpy = connection;
-  scre = screen;
+  xcb_flush(dpy);
 
   xcb_generic_event_t *event;
-  while ((event = xcb_wait_for_event(connection))) {
+  while ((event = xcb_wait_for_event(dpy))) {
     switch (event->response_type) {
     case XCB_EXPOSE: {
       xcb_expose_event_t *expose = (xcb_expose_event_t *)event;
       break;
     }
     case XCB_BUTTON_PRESS: {
-      printf("\n\n\n\n\n\nttttttttttttttttttt");
       handleButtonPress(event);
       break;
     }
