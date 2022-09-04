5623f9aec54eabfec0fc1c3a673567a7d67ea604
andrecaspar <23707164+andrecaspar@users.noreply.github.com>
2022-09-03 22:19:39 -0300

    Add files via upload

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 all: make
 
 make:
-	g++ test.cpp -o test `pkg-config gtkmm-4.0 x11 fontconfig xft --cflags --libs` ; pkill test
+	g++ test.cpp -o test `pkg-config gtkmm-4.0 xcb xcb-cursor --cflags --libs` -pthread
--- test.cpp
+++ test.cpp
@@ -1,7 +1,11 @@
 #include <gtkmm.h>
-#include <X11/Xlib.h>
-#include <X11/cursorfont.h>
+#include <inttypes.h>
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <thread>
+#include <xcb/xcb.h>
+#include <xcb/xcb_cursor.h>
 
 class WindowMain : public Gtk::Window {
 public:
@@ -38,51 +42,147 @@ private:
   Gtk::Button button1;
 };
 
-int main(int argc, char *argv[]) {
-  Display *dpy;
-  Window root;
-  XWindowAttributes attr;
-  XButtonEvent start;
-  XEvent ev;
+void test(int argc, char *argv[]) {
+  auto application = Gtk::Application::create("test");
+  application->make_window_and_run<WindowMain>(argc, argv);
+}
 
-  if (!(dpy = XOpenDisplay(0)))
-    return 1;
+void test2() {
+  xcb_connection_t *connection = xcb_connect(NULL, NULL);
 
-  root = DefaultRootWindow(dpy);
+  /* Get the first screen */
+  xcb_screen_t *screen =
+      xcb_setup_roots_iterator(xcb_get_setup(connection)).data;
 
-  Cursor cursor = XCreateFontCursor(dpy, XC_left_ptr);
-  XDefineCursor(dpy, root, cursor);
+  /* Create the window */
+  xcb_window_t window = xcb_generate_id(connection);
 
-  XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("F1")), Mod1Mask, root,
-           True, GrabModeAsync, GrabModeAsync);
-  XGrabButton(dpy, 1, Mod1Mask, root, True, ButtonPressMask, GrabModeAsync,
-              GrabModeAsync, None, None);
-  XGrabButton(dpy, 3, Mod1Mask, root, True, ButtonPressMask, GrabModeAsync,
-              GrabModeAsync, None, None);
+  uint32_t mask = XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK;
+  uint32_t values[2] = {
+      screen->white_pixel,
+      XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_BUTTON_PRESS |
+          XCB_EVENT_MASK_BUTTON_RELEASE | XCB_EVENT_MASK_POINTER_MOTION |
+          XCB_EVENT_MASK_ENTER_WINDOW | XCB_EVENT_MASK_LEAVE_WINDOW |
+          XCB_EVENT_MASK_KEY_PRESS | XCB_EVENT_MASK_KEY_RELEASE};
 
-  for (;;) {
-    auto application = Gtk::Application::create("test");
-    application->make_window_and_run<WindowMain>(argc, argv);
-    XNextEvent(dpy, &ev);
-    if (ev.type == KeyPress && ev.xkey.subwindow != None)
-      XRaiseWindow(dpy, ev.xkey.subwindow);
-    else if (ev.type == ButtonPress && ev.xbutton.subwindow != None) {
-      XGrabPointer(dpy, ev.xbutton.subwindow, True,
-                   PointerMotionMask | ButtonReleaseMask, GrabModeAsync,
-                   GrabModeAsync, None, None, CurrentTime);
-      XGetWindowAttributes(dpy, ev.xbutton.subwindow, &attr);
-      start = ev.xbutton;
-    } else if (ev.type == MotionNotify) {
-      int xdiff, ydiff;
-      while (XCheckTypedEvent(dpy, MotionNotify, &ev))
-      xdiff = ev.xbutton.x_root - start.x_root;
-      ydiff = ev.xbutton.y_root - start.y_root;
-      XMoveResizeWindow(dpy, ev.xmotion.window,
-                        attr.x + (start.button == 1 ? xdiff : 0),
-                        attr.y + (start.button == 1 ? ydiff : 0),
-                        MAX(1, attr.width + (start.button == 3 ? xdiff : 0)),
-                        MAX(1, attr.height + (start.button == 3 ? ydiff : 0)));
-    } else if (ev.type == ButtonRelease)
-      XUngrabPointer(dpy, CurrentTime);
+  xcb_create_window(connection, 0,                 /* depth               */
+                    window, screen->root,          /* parent window       */
+                    0, 0,                          /* x, y                */
+                    150, 150,                      /* width, height       */
+                    10,                            /* border_width        */
+                    XCB_WINDOW_CLASS_INPUT_OUTPUT, /* class               */
+                    screen->root_visual,           /* visual              */
+                    mask, values);                 /* masks */
+
+  /* Map the window on the screen */
+  xcb_map_window(connection, window);
+
+  xcb_cursor_context_t *pcursorctx;
+  xcb_cursor_context_new(connection, screen, &pcursorctx);
+  xcb_cursor_t cursor = xcb_cursor_load_cursor(pcursorctx, "left_ptr");
+  xcb_change_window_attributes(connection, screen->root, XCB_CW_CURSOR,
+                               &cursor);
+
+  xcb_flush(connection);
+
+  xcb_generic_event_t *event;
+  while ((event = xcb_wait_for_event(connection))) {
+    switch (event->response_type & ~0x80) {
+    case XCB_EXPOSE: {
+      xcb_expose_event_t *expose = (xcb_expose_event_t *)event;
+
+      printf("Window %" PRIu32
+             " exposed. Region to be redrawn at location (%" PRIu16 ",%" PRIu16
+             "), with dimension (%" PRIu16 ",%" PRIu16 ")\n",
+             expose->window, expose->x, expose->y, expose->width,
+             expose->height);
+      break;
+    }
+    case XCB_BUTTON_PRESS: {
+      xcb_button_press_event_t *bp = (xcb_button_press_event_t *)event;
+
+      xcb_configure_window(connection,bp->event, XCB_CONFIG_WINDOW_X, &bp->root_x);
+
+      switch (bp->detail) {
+      case 4:
+        printf("Wheel Button up in window %" PRIu32 ", at coordinates (%" PRIi16
+               ",%" PRIi16 ")\n",
+               bp->event, bp->event_x, bp->event_y);
+        break;
+      case 5:
+        printf("Wheel Button down in window %" PRIu32
+               ", at coordinates (%" PRIi16 ",%" PRIi16 ")\n",
+               bp->event, bp->event_x, bp->event_y);
+        break;
+      default:
+        printf("Button %" PRIu8 " pressed in window %" PRIu32
+               ", at coordinates (%" PRIi16 ",%" PRIi16 ")\n",
+               bp->detail, bp->event, bp->event_x, bp->event_y);
+        break;
+      }
+      break;
+    }
+    case XCB_BUTTON_RELEASE: {
+      xcb_button_release_event_t *br = (xcb_button_release_event_t *)event;
+
+      printf("Button %" PRIu8 " released in window %" PRIu32
+             ", at coordinates (%" PRIi16 ",%" PRIi16 ")\n",
+             br->detail, br->event, br->event_x, br->event_y);
+      break;
+    }
+    case XCB_MOTION_NOTIFY: {
+      xcb_motion_notify_event_t *motion = (xcb_motion_notify_event_t *)event;
+
+      printf("Mouse moved in window %" PRIu32 ", at coordinates (%" PRIi16
+             ",%" PRIi16 ")\n",
+             motion->event, motion->event_x, motion->event_y);
+      break;
+    }
+    case XCB_ENTER_NOTIFY: {
+      xcb_enter_notify_event_t *enter = (xcb_enter_notify_event_t *)event;
+
+      printf("Mouse entered window %" PRIu32 ", at coordinates (%" PRIi16
+             ",%" PRIi16 ")\n",
+             enter->event, enter->event_x, enter->event_y);
+      break;
+    }
+    case XCB_LEAVE_NOTIFY: {
+      xcb_leave_notify_event_t *leave = (xcb_leave_notify_event_t *)event;
+
+      printf("Mouse left window %" PRIu32 ", at coordinates (%" PRIi16
+             ",%" PRIi16 ")\n",
+             leave->event, leave->event_x, leave->event_y);
+      break;
+    }
+    case XCB_KEY_PRESS: {
+      xcb_key_press_event_t *kp = (xcb_key_press_event_t *)event;
+
+      printf("Key pressed in window %" PRIu32 "\n", kp->event);
+      break;
+    }
+    case XCB_KEY_RELEASE: {
+      xcb_key_release_event_t *kr = (xcb_key_release_event_t *)event;
+
+      printf("Key released in window %" PRIu32 "\n", kr->event);
+      break;
+    }
+    default:
+      /* Unknown event type, ignore it */
+      printf("Unknown event: %" PRIu8 "\n", event->response_type);
+      break;
+    }
+
+    free(event);
   }
+}
+
+int main(int argc, char *argv[]) {
+
+  std::thread threadTest(test, argc, argv);
+  std::thread threadTest2(test2);
+
+  threadTest.join();
+  threadTest2.join();
+
+  return 0;
 }
